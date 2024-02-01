4b6c7a9a906580c39f514329f75aca98334e397f
igorChechura <igorchechura@gmail.com>
2024-02-01 23:52:07 +0300

    getTransactionFiles function

--- app/App.php
+++ app/App.php
@@ -1,3 +1,18 @@
 <?php
 
 declare(strict_types=1);
+
+function getTransactionFiles(): array
+{
+    $files = [];
+
+    foreach (scandir(FILES_PATH) as $file) {
+        if (is_dir($file)) {
+            continue;
+        }
+
+        $files[] = $file;
+    }
+
+    return $files;
+}
