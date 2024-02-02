76671ab0dd43cee34a5b37bd914215f026c4eb75
igorChechura <igorchechura@gmail.com>
2024-02-02 23:46:26 +0300

    getTransactions function

--- app/App.php
+++ app/App.php
@@ -16,3 +16,22 @@ function getTransactionFiles(string $dirPath): array
 
     return $files;
 }
+
+function getTransactions(string $fileName): array
+{
+    if(! file_exists($fileName)) {
+        trigger_error('File "' . $fileName . '" does not exist.', E_USER_ERROR);
+    }
+
+    $file = fopen($fileName, 'r');
+
+    fgetcsv($file);
+
+    $transactions = [];
+
+    while (($transaction = fgetcsv($file)) !== false) {
+        $transactions[] = $transaction;
+    }
+
+    return $transactions;
+}
--- public/index.php
+++ public/index.php
@@ -12,4 +12,10 @@ require APP_PATH . 'App.php';
 
 $files = getTransactionFiles(FILES_PATH);
 
-var_dump($files);
+$transactions = [];
+foreach($files as $file) {
+    $transactions = array_merge($transactions, getTransactions($file));
+}
+echo '<pre>';
+print_r($transactions);
+echo '</pre>';
