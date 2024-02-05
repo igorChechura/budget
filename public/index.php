82534569c59ff15ec8426d2aadf5bd6697d94d08
igorChechura <igorchechura@gmail.com>
2024-02-05 23:58:33 +0300

    totals draw view

--- public/index.php
+++ public/index.php
@@ -17,4 +17,6 @@ foreach($files as $file) {
     $transactions = array_merge($transactions, getTransactions($file, 'extractTransaction'));
 }
 
+$totals = calculateTotals($transactions);
+
 require VIEWS_PATH . 'transactions.php';
